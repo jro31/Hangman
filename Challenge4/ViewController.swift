6ac61535aac943964c8e5f69432344f6e5bea7a1
Jethro Williams <jethrowilliams4@hotmail.com>
2018-08-06 21:24:37 +0100

    Add playing game function to amend the ??? word for when students select correct letters.

--- Challenge4/ViewController.swift
+++ Challenge4/ViewController.swift
@@ -6,9 +6,24 @@ class ViewController: UIViewController {
     @IBOutlet var gameWordLabel: UILabel!
     @IBOutlet var livesLabel: UILabel!
     
+    var chosenLetterString = ""
+    var chosenLetterCharacter = Array<Character>()
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        chosenLetterString = sender.titleLabel!.text!
+        chosenLetterCharacter = Array(chosenLetterString)
+        print("Chosen letter character: \(chosenLetterCharacter)")
+        
+        playingGame()
+    }
+    
     var allWords = [String]()
     var questionMarks = ""
-    var livesRemaining = 7
+    var livesRemaining = 10
+    var gameWord = ""
+    var lettersArray = Array<Character>()
+    var startingGameWordLabel = ""
+    var questionMarksArray = Array<Character>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,22 +37,48 @@ class ViewController: UIViewController {
         }
         
         startGame()
+        
     }
     
     func startGame() {
         allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
-        let gameWord = allWords[0]
+        gameWord = allWords[0].uppercased()
         print("Gameword: \(gameWord)")
-        let lettersArray = Array(gameWord) //This breaks-down 'gameWord' into an array of characters
+        lettersArray = Array(gameWord) //This breaks-down 'gameWord' into an array of characters
+        print("Letters array: \(lettersArray)")
         
         for _ in lettersArray {
             questionMarks += "?"
         }
-        let questionMarksArray = Array(questionMarks) //This breaks-down 'questionMarks' into an array of question marks
-        let startingGameWordLabel = String(questionMarksArray)
+        questionMarksArray = Array(questionMarks) //This breaks-down 'questionMarks' into an array of question marks
+        startingGameWordLabel = String(questionMarksArray)
         
         gameWordLabel.text = startingGameWordLabel
         livesLabel.text = "\(livesRemaining)"
+    }
+    
+    func playingGame() {
+        
+        var updatedGameWord: [Character]
+        var updatedGameWordText: String
+        
+        for letters in lettersArray {
+            if letters == chosenLetterCharacter[0] {
+                let correctLetterPosition = lettersArray.index(of: chosenLetterCharacter[0])!
+                
+                if gameWordLabel.text == startingGameWordLabel {
+                    updatedGameWord = questionMarksArray
+                    updatedGameWord[correctLetterPosition] = chosenLetterCharacter[0]
+                    updatedGameWordText = String(updatedGameWord)
+                    gameWordLabel.text = updatedGameWordText
+                } else {
+                    var veryUpdatedGameWord = Array(gameWordLabel.text!)
+                    veryUpdatedGameWord[correctLetterPosition] = chosenLetterCharacter[0]
+                    let veryUpdatedGameWordText = String(veryUpdatedGameWord)
+                    gameWordLabel.text = veryUpdatedGameWordText
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
